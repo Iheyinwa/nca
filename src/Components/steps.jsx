9e0bf18d3b434797785bd2c8faa866b1cfa48751
Iheyinwa <caruogu@outlook.com>
2024-09-01 04:08:23 +0100

    Updated steps

--- src/Components/steps.jsx
+++ src/Components/steps.jsx
@@ -9,14 +9,13 @@ const Steps = () => {
           Follow the instructions to cast your vote;
         </p>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 lg:grid-rows-4 w-[90%] mx-auto my-4">
+      <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 w-[90%] mx-auto my-4">
         <StepsCard
           count="1"
-          content="Let your living waters Flow out of me Let your Holy Spirit overshadow me
-        As it was in the beginning So let it be now"
+          content="Click Vote"
           className="lg:col-start-6 lg:col-span-2 lg:row-start-1 lg:row-end-2"
         />
-        <div className="w-fit h-full lg:col-span-3 lg:row-start-1 lg:row-end-4">
+        <div className="w-fit h-full lg:col-span-3 lg:row-start-1 lg:row-end-6">
           <img
             src={Logo}
             alt="NCA Steps"
@@ -27,15 +26,23 @@ const Steps = () => {
         </div>
         <StepsCard
           count="2"
-          content="Let your living waters Flow out of me Let your Holy Spirit overshadow me
-        As it was in the beginning So let it be now"
+          content="Select your District"
           className="lg:col-start-1 lg:col-span-2 lg:row-start-2 lg:row-end-3"
         />
         <StepsCard
           count="3"
-          content="Let your living waters Flow out of me Let your Holy Spirit overshadow me
-        As it was in the beginning So let it be now"
+          content="Click on the group you want to vote"
           className="lg:row-start-3 lg:row-span-1 lg:col-start-6 lg:col-span-2 "
+        />
+        <StepsCard
+          count="4"
+          content="Put in your email address and the amount you want to pay"
+          className="lg:row-start-4 lg:row-span-1 lg:col-start-1 lg:col-span-2 "
+        />
+        <StepsCard
+          count="5"
+          content="Select your preferred payment method and make your payment"
+          className="lg:row-start-5 lg:row-span-1 lg:col-start-6 lg:col-span-2 "
         />
       </div>
     </section>
